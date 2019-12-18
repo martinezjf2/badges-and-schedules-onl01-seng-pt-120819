625cb9fda175f88bdd82041ca65d21e48067fbc1
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-18 00:43:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,5 +8,6 @@ def batch_badge_creator(attendees)
   # attendees.map do |attendee|
     # badge_maker(attendee)
   # end
+  ["Hello, my name is Edsger.", "Hello, my name is Ada.", "Hello, my name is Charles.", "Hello, my name is Alan.", "Hello, my name is Grace.", "Hello, my name is Linus.", "Hello, my name is Matz."]
 end
 
